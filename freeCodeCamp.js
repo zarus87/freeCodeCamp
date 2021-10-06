2cfaa30b738e58a636591bccaa6ff477b3962beb
Kirill <86402812+zarus87@users.noreply.github.com>
2021-10-06 10:21:49 +0200

    20
    первые 20 заданий по курсу "JavaScript алгоритмы и структуры данных"

---
+++ freeCodeCamp.js
@@ -0,0 +1,90 @@
+//В этом файле указаны все мои решения в курсу "JavaScript алгоритмы и структуры данных" на freeCodeCamp!
+
+
+1.// Это первый вид комментария 
+/*Это второй вид комментария */
+
+2. var myName;
+
+3.var a;
+a = 7;
+
+4.var a;
+a = 7;
+var b;
+b = a;
+
+5. var a = 9;
+
+6. var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+7. var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+8. var sum = 10 + 10;
+
+9. var difference = 45 - 33;
+
+10. var product = 8 * 10;
+
+11. var quotient = 66 / 33;
+
+12. var myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+13. var myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+14. var myDecimal = 5.7;
+
+15. var product = 2.5 * 2.0;
+
+16. var quotient = 4.4 / 2.0; // Change this line
+
+17. var remainder = 11 % 3;
+
+18. var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+19. var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+20. var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+21. 
