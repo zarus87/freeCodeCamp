09791af03673d5e6540b747354d06e2b26c98aad
Kirill <86402812+zarus87@users.noreply.github.com>
2021-10-08 19:58:52 +0200

    63 + 6 OOP

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -402,7 +402,112 @@ function testStrictNotEqual(val) {
 
 testStrictNotEqual(10);
 
-57. 
+57. function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+58. function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+59. function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+60. function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+61. function testLogicalAnd(val) {
+  // Only change code below this line
+
+  
+    if (val >=25 && val <= 50) {
+      return "Yes";
+    }
+  
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+62. function testLogicalOr(val) {
+  // Only change code below this line
+
+  
+
+  if (val >20 ||  val <10) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+63. function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+64. 
+
 
 
 
