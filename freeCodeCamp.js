39d3f68cc7dc8a0caf39f1af9f792789e715b00f
Kirill <86402812+zarus87@users.noreply.github.com>
2021-10-08 13:44:32 +0200

    55 + 6 OOP

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -184,7 +184,215 @@ var myArray = [18,64,99];
 myArray[0] = 45
 // Only change code below this line
 
-41.
+41. var myArray = 
+[[1,2,3],
+ [4,5,6], 
+ [7,8,9], 
+ [[10,11,12], 
+ 13, 14]];
+
+var myData = myArray[2][1];
+
+42. // Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+myArray.push(["dog", 3]);// Only change code below this li
+
+43. // Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
+
+44. // Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
+
+45. // Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul",35]);
+
+// Only change code below this line
+
+46. var myList = [
+  ["chocolate", 5],
+  ["ovsanka", 1],
+  ["grechka", 3],
+  ["tvorog", 2],
+  ["sir", 1]
+  
+  ];
+
+  47. function reusableFunction() {
+    console.log ("Hi World");
+    }
+    reusableFunction();
+
+    48. function functionWithArgs(wer,dfg) {
+  
+      console.log(wer+dfg);
+    }
+    
+    functionWithArgs(3,4);
+
+    49. var myGlobal = 10;// Declare the myGlobal variable below this line
+
+
+    function fun1() {
+      oopsGlobal = 5// Assign 5 to oopsGlobal Here
+    
+    }
+    
+    // Only change code above this line
+    
+    function fun2() {
+      var output = "";
+      if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+      }
+      if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+      }
+      console.log(output);
+    }
+
+    50. function myLocalScope() {
+
+      var myVar = "внутри"// Only change code below this line
+    
+      console.log('inside myLocalScope', myVar);
+    }
+    myLocalScope();
+    
+    // Run and check the console
+    // myVar is not defined outside of myLocalScope
+    console.log('outside myLocalScope', myVar);
+
+    51. // Setup
+    var outerWear = "T-Shirt";
+    
+    function myOutfit() {
+      // Only change code below this line
+    var outerWear = "sweater";
+    
+    
+      // Only change code above this line
+      return outerWear;
+    }
+    
+    myOutfit();
+
+    52. function timesFive(num) {
+      return num * 5;
+    }
+ 53. // Setup
+ var sum = 0;
+ 
+ function addThree() {
+   sum = sum + 3;
+ }
+ 
+ // Only change code below this line
+ function addFive(){ 
+   sum = sum +5;
+ }
+ // Only change code above this line
+ 
+ addThree();
+ addFive();
+ 
+ 54. // Setup
+ var processed = 0;
+ 
+ function processArg(num) {
+   return (num + 3) / 5;
+ }
+ processed = processArg(7);
+ // Only change code below this line
+
+ 55. function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  return arr.shift();
+
+  return item;
+  // Only change code above this line
+  
+
+}
+
+// Setup
+var testArr = [1,2,3,4,5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+56. function welcomeToBooleans() {
+
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
+
+57. function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue) {
+  return "Yes, that was true";
+} 
+return "No, that was false";
+
+  // Only change code above this line
+
+}
+
+52. // Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+53. // Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+54. // Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+55. // Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+56. 
 
 
 
