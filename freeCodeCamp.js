17e16e5bad1aacf96ce59080a3e84eb323e2e63f
Kirill <86402812+zarus87@users.noreply.github.com>
2021-10-09 16:19:16 +0200

    70 + 6 OOP

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -506,7 +506,156 @@ testLogicalOr(15);
 
 testElse(4);
 
-64. 
+64. function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+
+  return "Between 5 and 10";}
+}
+
+testElseIf(5);
+
+65. function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+66. function testSize(num) {
+  // Only change code below this line
+if (num <5) {
+  return "Tiny"
+} else if  (num < 10) {
+   return "Small"
+} else if (num <15) {
+  return "Medium"
+} else if (num <20) {
+  return "Large"
+} else (num >= 20) ;{
+  return "Huge"
+}
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+67. var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+if (strokes == 1) {
+  return names[0]
+} else if (strokes <= par - 2) {
+  return names[1]
+} else if (strokes == par - 1) {
+  return names[2]
+} else if (strokes == par) {
+  return names[3]
+} else if (strokes == par + 1) {
+  return names[4]
+} else if (strokes == par + 2) {
+  return names[5]
+} else  (strokes >= par + 3); {
+  return names[6]
+} 
+
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+68.  
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+switch(val) {
+case 1 :
+answer = "alpha";
+break;
+case 2:
+answer = "beta";
+break;
+case 3:
+answer = "gamma";
+break;
+case 4:
+answer = "delta";
+break;
+
+}
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+69. function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+switch (val){
+  case "a":
+  answer = "apple";
+  break;
+  case "b":
+  answer = "bird";
+  break;
+  case "c":
+  answer = "cat";
+  break;
+  default :
+  answer = "stuff";
+  break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+70. function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+switch (val){
+  case 1:
+  case 2:
+  case 3:
+  answer = "Low";
+  break;
+  case 4:
+  case 5:
+  case 6:
+  answer = "Mid";
+  break;
+  case 7:
+  case 8:
+  case 9:
+  answer = "High";
+  break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+71. 
 
 
 
