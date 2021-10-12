e3d929d2b071eeaa9e9ca078e7e3e6810e03f1a7
Kirill <86402812+zarus87@users.noreply.github.com>
2021-10-12 11:52:31 +0200

    94, es6 12, 6 oop

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -978,7 +978,108 @@ cc(2); cc(3); cc(7); cc('K'); cc('A');
   }
   catTalk();
 
-  2. 
+  2. function checkScope() {
+  let i = 'function scope';
+  if (true) {
+   let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+ 3. function printManyTimes(str) {
+
+  // Only change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
+
+4. const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+s[0] = 2;
+s[1] = 5;
+s[2] = 7;
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+
+5. function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+6. const magic = () =>
+new Date();
+
+7. const myConcat = (arr1, arr2) =>
+arr1.concat(arr2);
+
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+8. // Only change code below this line
+const increment = (number = 1, value = 1) => number + value;
+// Only change code above this line
+
+9. const sum = (...args) => {
+  
+  return args.reduce((a, b) => a + b, 0);
+}
+10. const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+
+11. const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
+
+
+// Only change code above this line
+
+12. const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  
+const {today: highToday, tomorrow:highTomorrow} = HIGH_TEMPERATURES; 
+
+// Only change code above this line
+
+13. 
 
 
 
