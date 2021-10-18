4c5679e1a2464a8983de9652fd1bc0e46b5d115a
Kirill <86402812+zarus87@users.noreply.github.com>
2021-10-18 16:41:00 +0300

    bjs 94; es6 20; re 33; d 12;SD 2; oop 6;

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1144,7 +1144,30 @@ class Vegetable {
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); 
 
-21. 
+21. // Only change code below this line
+class Thermostat {
+  constructor(far){
+   this.far = far;
+  }
+
+  get temperature() {
+    return 5/9 * (this.far - 32);  // перевод в цельсии
+  }
+
+  set temperature(celsius) {
+        this.far = celsius * 9.0 / 5 + 32; // перевод в фаренгейт
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // задаём градусы фаренгейта
+let temp = thermos.temperature; // 24.44 in Celsius
+console.log(temp)
+thermos.temperature = 26;    
+temp = thermos.temperature; // 26 in Celsius
+console.log(temp)
+
+22.
 
 
 
