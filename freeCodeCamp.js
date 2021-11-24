42616a379d1d5fa66a069c7202b98861c7865e83
Kirill <86402812+zarus87@users.noreply.github.com>
2021-11-24 12:30:52 +0300

    bjs 103; es6 31; re 33; d 12;SD 2; oop 22;

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1708,4 +1708,21 @@ console.log(result)
    };
    let beagle = new Dog();
 
-   22. 
+   22. function Bird() { }
+
+   Bird.prototype.fly = function() { return "I am flying!"; };
+   
+   function Penguin() { }
+   Penguin.prototype = Object.create(Bird.prototype);
+   Penguin.prototype.constructor = Penguin;
+   
+   // Only change code below this line
+   Penguin.prototype.fly = function() {
+     return "Alas, this is a flightless bird.";  // изменяю унаследованые свойства
+   };
+   // Only change code above this line
+   
+   let penguin = new Penguin();
+   console.log(penguin.fly());
+
+   23. 
