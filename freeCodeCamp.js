a11e6f935e8aed76974da989381d69cf672059ee
Kirill <86402812+zarus87@users.noreply.github.com>
2021-12-05 11:29:41 +0300

    bjs 103; es6 31; re 33; d 12;SD 11; oop 26; fp 2;

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1639,7 +1639,20 @@ if (arr.indexOf(elem) >= 0) {
 }
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
 
-11.  
+11.  function filteredArray(arr, elem) {
+  let newArr = [];
+  // Only change code below this line
+ for (let i = 0; i < arr.length; i++) {            // бзззз
+    if (arr[i].indexOf(elem) == -1) {
+      
+      newArr.push(arr[i]); 
+    }
+  }
+  // Only change code above this line
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
 
 
 
