36aa53fd6da18c6c9db7cab1f6e5ae62118a0ca7
Kirill <86402812+zarus87@users.noreply.github.com>
2021-12-14 11:23:04 +0300

    bjs 113; es6 31; re 33; d 12;SD 20; oop 26; fp 2;

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -880,10 +880,54 @@ function checkSign(num) {
     checkSign(10);
 
 104.
+сonst contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
 
+function lookUpProfile(name, prop) {
+ for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
 
 105.
-
+function randomFraction() {
+var result = 0;
+  while (result === 0) {
+    result = Math.random();
+  }
+  return result;
+}
 
 106.
 function randomWholeNum() {
