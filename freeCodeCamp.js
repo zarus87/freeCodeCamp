5499c5cef52172aeff45a3f2ebab40f82dbe37b7
Kirill <86402812+zarus87@users.noreply.github.com>
2021-12-18 18:24:45 +0300

    bjs 113; es6 31; re 33; d 12;SD 20; oop 26; fp 3;

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1135,7 +1135,8 @@ const arr = removeFirstTwo(source);
 const half = ({max, min}) => (max+min) /2;  //как параметры max и min попадают из константы stats попадают в  константу half совершенно не понятно!
 
 
-17.const result = {
+17.
+const result = {
   success: ["max-length", "no-amd", "prefer-arrow-functions"],
   failure: ["no-var", "var-on-top", "linebreak"],
   skipped: ["no-extra-semi", "no-dup-keys"]
@@ -2121,3 +2122,47 @@ let funModule = (function () {
   }
   
   console.log(mixedNumbers(['IV', 5, 'six']));
+
+
+    //// Раздел функционального програмирования
+    
+    1.
+
+
+    2.
+
+
+    3.
+    // tabs is an array of titles of each site open within the window
+const Window = function(tabs) {
+  this.tabs = tabs; // We keep a record of the array inside the object
+};
+// When you join two windows into one window
+Window.prototype.join = function(otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+// When you open a new tab at the end
+Window.prototype.tabOpen = function(tab) {
+  this.tabs.push('new tab'); // Let's open a new tab for now
+  return this;
+};
+// When you close a tab
+Window.prototype.tabClose = function(index) {
+  const tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
+  const tabsAfterIndex = this.tabs.splice(1); // в скобках 1 надо
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+  return this;
+ };
+// Let's create three browser windows
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+// Now perform the tab opening, closing, and other operations
+const finalTabs = socialWindow
+  .tabOpen() // Open a new tab for cat memes
+  .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+  .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
+
+
